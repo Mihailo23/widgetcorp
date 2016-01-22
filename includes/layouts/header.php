79c5a4aac48fcec86b4d72da4f3420fb44945edc
Mica <mica@mica.com>
2016-01-22 15:35:50 +0100

    started building the cms, made header.php and footer.php include in files

---
+++ includes/layouts/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Widget Corp</title>
+	<link rel="stylesheet" href="stylesheets/public.css">
+</head>
+<body>
+	<div id="header">
+		<h1>Widget Corp</h1>
+	</div>
