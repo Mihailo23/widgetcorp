e1e6d1902eab2cccc2bec92de930adcd0b5adc1b
Mica <mica@mica.com>
2016-06-06 17:21:38 +0200

    needed commit

--- public/admin.php
+++ public/admin.php
@@ -8,7 +8,7 @@
 		<p>Welcome to the admin area.</p>
 		<ul>
 			<li><a href="manage_content.php">Manage Website Content</a></li>
-			<li><a href="manage_admins.php">Manage Admin Useres</a></li>
+			<li><a href="manage_admins.php">Manage Admin Users</a></li>
 			<li><a href="logout.php">Logout</a></li>
 		</ul>
 	</div>
